1bd3edb2d85f922bcf7c98650ccb92d51945e36a
begimov <begimov@gmail.com>
2018-03-04 17:19:50 +0300

    enviromental vars loaded using dotenv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,3 +3,12 @@
 session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+try {
+    $dotenv = (new Dotenv\Dotenv(__DIR__ . '/..//'))->load();
+} catch (Dotenv\Exception\InvalidPathException $e) {
+    //
+}
+
+var_dump(getenv('APP_NAME'));
+
