d0aa270ec379e540e8150d77a429fcaf76741f08
begimov <begimov@gmail.com>
2018-03-10 16:34:24 +0300

    base_path helper implemented

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,8 @@
 <?php
 
-function test()
-{
-    return 'test';
+if (!function_exists('base_path')) {
+    function base_path($path = '')
+    {
+        return __DIR__ . '/..//' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
 }
