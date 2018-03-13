7dedaddec21c91bef3d85cf93f78b53192d83c3c
begimov <begimov@gmail.com>
2018-03-13 06:04:42 +0300

    doctrine entity manager shared via container using Database service provider

--- app/Providers/DatabaseServiceProvider.php
+++ app/Providers/DatabaseServiceProvider.php
@@ -2,18 +2,33 @@
 
 namespace App\Providers;
 
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class DatabaseServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        //
+        EntityManager::class
     ];
 
     public function register()
     {
         $container = $this->getContainer();
 
-        //
+        $config = $container->get('config');
+
+        $container->share('db', function () use ($config) {
+            $emConfig = $config->get('db.' . env('DB_CONNECTION'));
+
+            $entityManager = EntityManager::create(
+                $emConfig, 
+                Setup::createAnnotationMetadataConfiguration(
+                    [base_path('app')],
+                    $config->get('app.debug')
+                )
+            );
+            return $entityManager;
+        });
     }
 }
