530baa430ca2f1b564494fbc7d8e3f7c1dd3e33a
begimov <begimov@gmail.com>
2018-03-13 05:49:29 +0300

    template DatabaseServiceProvider created

---
+++ app/Providers/DatabaseServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class DatabaseServiceProvider extends AbstractServiceProvider
+{
+    protected $provides = [
+        //
+    ];
+
+    public function register()
+    {
+        $container = $this->getContainer();
+
+        //
+    }
+}
