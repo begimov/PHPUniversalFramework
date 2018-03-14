19f7d4702193ecf6d2c76cb25c5ee6b22ea87541
begimov <begimov@gmail.com>
2018-03-14 15:23:25 +0300

    HomeController refactored, unused code removed

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,26 +3,19 @@
 namespace App\Controllers;
 
 use App\Views\View;
-use App\Models\User;
-use Doctrine\ORM\EntityManager;
 
 class HomeController
 {
     protected $view;
     protected $db;
 
-    public function __construct(View $view, EntityManager $db)
+    public function __construct(View $view)
     {
         $this->view = $view;
-        $this->db = $db;
     }
 
     public function index($request, $response)
     {
-        $user = $this->db->getRepository(User::class)->find(1);
-
-        return $this->view->render($response, 'home.twig', [
-            'user' => $user
-        ]);
+        return $this->view->render($response, 'home.twig');
     }
 }
