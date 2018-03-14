213264b6cf8da99c6a710e7ee9835dc0c15162e9
begimov <begimov@gmail.com>
2018-03-14 17:14:20 +0300

    login template ui created

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
-$route->get('/', 'App\Controllers\HomeController::index')->setName('home');
+// $route->get('/', 'App\Controllers\HomeController::index')->setName('home');
+$route->get('/', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
+
+// $route->group('/auth', function ($route) {
+//     $route->get('/login', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
+// });
