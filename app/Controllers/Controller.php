61f4abe20e67009260e3c87a9bfafca279ba2c48
begimov <begimov@gmail.com>
2018-03-16 17:21:00 +0300

    basic validate controller method implemented using valitron

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -2,10 +2,21 @@
 
 namespace App\Controllers;
 
+use Valitron\Validator;
+
 abstract class Controller
 {
-    public function validate($request, $rules)
+    public function validate($request, array $rules)
     {
-        dump($request, $rules);
+        $validator = new Validator($request->getParsedBody());
+
+        $validator->mapFieldsRules($rules);
+
+        if (!$validator->validate()) {
+            dump('failed');
+            die();
+        }
+
+        return $request->getParsedBody();
     }
 }
