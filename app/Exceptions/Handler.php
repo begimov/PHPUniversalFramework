1f85e81af21cf551fe4801960ced8404d34fb64c
begimov <begimov@gmail.com>
2018-03-17 09:16:51 +0300

    basic exception Handler class implemented

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,5 +4,15 @@ namespace App\Exceptions;
 
 class Handler
 {
-    //
+    protected $exception;
+
+    public function __construct(\Exception $exception)
+    {
+        $this->exception = $exception;
+    }
+
+    public function respond()
+    {
+        dump($this->exception);
+    }
 }
