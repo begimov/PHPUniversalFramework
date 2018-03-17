56744dc52a5b635fc631d9e1c44333fefaf2aa76
begimov <begimov@gmail.com>
2018-03-17 12:10:51 +0300

    SessionServiceProvider register method implemented to share ISession interface implementation

--- app/Providers/SessionServiceProvider.php
+++ app/Providers/SessionServiceProvider.php
@@ -2,18 +2,22 @@
 
 namespace App\Providers;
 
+use App\Session\Session;
+use App\Session\ISession;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class SessionServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        //
+        ISession::class
     ];
 
     public function register()
     {
         $container = $this->getContainer();
 
-        $container->share();
+        $container->share(ISession::class, function () {
+            return new Session();
+        });
     }
 }
