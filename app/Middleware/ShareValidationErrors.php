3af09693b4fcc2f2eb746060af66ba8980f66f30
begimov <begimov@gmail.com>
2018-03-23 08:21:12 +0300

    test middleware class updated

--- app/Middleware/ShareValidationErrors.php
+++ app/Middleware/ShareValidationErrors.php
@@ -1,9 +1,12 @@
 <?php
 
+namespace App\Middleware;
+
 class ShareValidationErrors
 {
     public function __invoke($request, $response, callable $next)
     {
-        //
+        dump('ShareValidationErrors');
+        return $next($request, $response);
     }
 }
