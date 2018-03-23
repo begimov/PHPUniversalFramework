8c1a51cb7100ce42edc2e65c9dceedf338b82ebc
begimov <begimov@gmail.com>
2018-03-23 10:35:08 +0300

    injecting View in ShareValidationErrors using auto-wiring

--- app/Middleware/ShareValidationErrors.php
+++ app/Middleware/ShareValidationErrors.php
@@ -2,8 +2,17 @@
 
 namespace App\Middleware;
 
+use App\Views\View;
+
 class ShareValidationErrors
 {
+    protected $view;
+
+    public function __construct(View $view)
+    {
+        $this->view = $view;
+    }
+
     public function __invoke($request, $response, callable $next)
     {
         return $next($request, $response);
