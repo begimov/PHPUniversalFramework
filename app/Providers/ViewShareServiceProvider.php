f4b181d253fcada06d3ccc77d0ed8d822339be5b
begimov <begimov@gmail.com>
2018-03-23 13:32:25 +0300

    template for ViewShareServiceProvider class created

---
+++ app/Providers/ViewShareServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class ViewShareServiceProvider extends AbstractServiceProvider
+{
+    protected $provides = [
+        //
+    ];
+
+    public function register()
+    {
+        //
+    }
+}
