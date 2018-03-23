e00857adf489f12eabd5e83e94b960816f9a847c
begimov <begimov@gmail.com>
2018-03-23 12:17:02 +0300

    sharing errors and old input via middleware with views

--- app/Middleware/ShareValidationErrors.php
+++ app/Middleware/ShareValidationErrors.php
@@ -3,21 +3,24 @@
 namespace App\Middleware;
 
 use App\Views\View;
+use App\Session\ISession;
 
 class ShareValidationErrors
 {
     protected $view;
+    protected $session;
 
-    public function __construct(View $view)
+    public function __construct(View $view, ISession $session)
     {
         $this->view = $view;
+        $this->session = $session;
     }
 
     public function __invoke($request, $response, callable $next)
     {
         $this->view->share([
-            'errors' => 'errors',
-            'oldInput' => 'oldInput',
+            'errors' => $this->session->get('errors', []),
+            'oldInput' => $this->session->get('oldInput', []),
         ]);
         return $next($request, $response);
     }
