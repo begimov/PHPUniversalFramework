65afe4d2a6f3e38c5acad40281a472fd15724b02
begimov <begimov@gmail.com>
2018-03-24 12:57:38 +0300

    basic AuthServiceProvider implemented

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,14 +4,20 @@ namespace App\Providers;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
+use App\Auth\Auth;
+
 class AuthServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        //
+        Auth::class
     ];
 
     public function register()
     {
         $container = $this->getContainer();
+
+        $container->share(Auth::class, function () use ($container) {
+            return new Auth();
+        });
     }
 }
