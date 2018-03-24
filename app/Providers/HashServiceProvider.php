6451b0de6564ae69ff5790b1c322686909d44aec
begimov <begimov@gmail.com>
2018-03-24 12:11:35 +0300

    Hash service provider implemented

--- app/Providers/HashServiceProvider.php
+++ app/Providers/HashServiceProvider.php
@@ -4,14 +4,20 @@ namespace App\Providers;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
+use App\Auth\Hashing\IHasher;
+use App\Auth\Hashing\BcryptHasher;
+
 class HashServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        //
+        IHasher::class
     ];
 
     public function register()
     {
         $container = $this->getContainer();
+        $container->share(IHasher::class, function () {
+            return new BcryptHasher;
+        });
     }
 }
