db16b88ff75ab893bf5776219b0c89e631d6fbe2
begimov <begimov@gmail.com>
2018-03-27 11:34:54 +0300

    passign authenticated user to home view added in Home controller

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,18 +3,21 @@
 namespace App\Controllers;
 
 use App\Views\View;
+use App\Auth\Auth;
 
 class HomeController
 {
     protected $view;
 
-    public function __construct(View $view)
+    public function __construct(View $view, Auth $auth)
     {
         $this->view = $view;
     }
 
     public function index($request, $response)
     {
-        return $this->view->render($response, 'home.twig');
+        return $this->view->render($response, 'home.twig', [
+            'user' => $auth->user()
+        ]);
     }
 }
