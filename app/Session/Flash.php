c00deb71f86cc12e1dae8c67df4e98a6d2d91888
begimov <begimov@gmail.com>
2018-03-31 08:51:06 +0300

    ISession dependency added to Flash class

--- app/Session/Flash.php
+++ app/Session/Flash.php
@@ -2,7 +2,14 @@
 
 namespace App\Session;
 
+use App\Session\ISession;
+
 class Flash
 {
-    //
+    protected $session;
+    
+    public function __construct(ISession $session)
+    {
+        $this->session = $session;
+    }
 }
