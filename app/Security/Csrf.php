3ed82fe0a3082f621a18303f9a70d0b991a760f6
begimov <begimov@gmail.com>
2018-04-05 10:35:10 +0300

    ISession instance pulled in Csrf class

--- app/Security/Csrf.php
+++ app/Security/Csrf.php
@@ -2,8 +2,17 @@
 
 namespace App\Security;
 
+use App\Session\ISession;
+
 class Csrf
 {
+    protected $session;
+
+    public function __construct(ISession $session)
+    {
+        $this->session = $session;
+    }
+
     public function hash()
     {
         return 'hash';
