ff41c7ce56e6a803627ea7fb8f7ed62a0fbedf1b
begimov <begimov@gmail.com>
2018-04-05 11:12:18 +0300

    logic to persist csrf token within a session implemented

--- app/Security/Csrf.php
+++ app/Security/Csrf.php
@@ -7,6 +7,7 @@ use App\Session\ISession;
 class Csrf
 {
     protected $session;
+    protected $persist = false;
 
     public function __construct(ISession $session)
     {
@@ -15,12 +16,34 @@ class Csrf
 
     public function token()
     {
+        if (!$this->needsToBeGenerated()) {
+            return $this->getFromSession();
+        }
+
         $this->session->set(
             $this->key(), 
             $token = bin2hex(random_bytes(32))
         );
 
         return $token;
+    }
+
+    protected function getFromSession()
+    {
+        return $this->session->get($this->key());
+    }
+
+    protected function needsToBeGenerated()
+    {
+        if ($this->shouldPersist()) {
+            return false;
+        }
+        return $this->session->exists($this->key());
+    }
+
+    protected function shouldPersist()
+    {
+        return $this->persist;
     }
 
     protected function key()
