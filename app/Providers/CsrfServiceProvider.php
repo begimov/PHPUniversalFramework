11c434ed4162f73961a6b33890303aa528265c60
begimov <begimov@gmail.com>
2018-04-05 10:21:36 +0300

    CsrfServiceProvider's register method implemented to share Crsf class instance

--- app/Providers/CsrfServiceProvider.php
+++ app/Providers/CsrfServiceProvider.php
@@ -2,16 +2,21 @@
 
 namespace App\Providers;
 
+use App\Security\Csrf;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class CsrfServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        //
+        Csrf::class
     ];
 
     public function register()
     {
         $container = $this->getContainer();
+
+        $container->share(Crsf::class, function () {
+            return new Csrf;
+        });
     }
 }
