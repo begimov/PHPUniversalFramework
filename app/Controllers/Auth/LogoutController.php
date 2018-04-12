ed3129bd53c525eb413a00daa175853174507514
begimov <begimov@gmail.com>
2018-04-12 10:22:28 +0300

    logout method implemented in LogoutController

--- app/Controllers/Auth/LogoutController.php
+++ app/Controllers/Auth/LogoutController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controllers\Auth;
 
+use App\Auth\Auth;
 use App\Controllers\Controller;
 
 class LogoutController extends Controller
 {
+    protected $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
     public function logout($request, $response)
     {
-        //
+        $this->auth->logout();
+        return redirect('/');
     }
 }
