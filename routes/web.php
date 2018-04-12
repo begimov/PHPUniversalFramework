5b129d756c6da2bec93709137b24a900820ebfab
begimov <begimov@gmail.com>
2018-04-12 11:57:00 +0300

    register web routes added

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ $route->group('/auth', function ($route) {
     $route->get('/login', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
     $route->post('/login', 'App\Controllers\Auth\LoginController::login');
     $route->post('/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
+
+    $route->get('/register', 'App\Controllers\Auth\RegisterController::index')->setName('auth.register');
+    $route->post('/register', 'App\Controllers\Auth\RegisterController::register');
 });
