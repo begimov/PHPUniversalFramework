a05bdcdc39c5dc469d2d4a283e58e4a6da2ca866
begimov <begimov@gmail.com>
2018-04-12 09:57:10 +0300

    template LogoutController created

---
+++ app/Controllers/Auth/LogoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Controllers\Controller;
+
+class LogoutController extends Controller
+{
+    public function logout($request, $response)
+    {
+        //
+    }
+}
