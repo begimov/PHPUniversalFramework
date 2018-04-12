14ef9cb8996ea507839bcf5dd51a647716651542
begimov <begimov@gmail.com>
2018-04-12 12:26:19 +0300

    template ValidationServiceProvider created

---
+++ app/Providers/ValidationServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+
+class ValidationServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    protected $provides = [
+        //
+    ];
+
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
