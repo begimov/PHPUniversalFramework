95834dbb4de034fa981a7830b670816f387ffbd2
begimov <begimov@gmail.com>
2018-04-18 12:40:48 +0300

    CookieJar's template class created

---
+++ app/Cookie/CookieJar.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Cookie;
+
+class CookieJar
+{
+    public function set()
+    {
+        # code...
+    }
+
+    public function get()
+    {
+        # code...
+    }
+
+    public function exists()
+    {
+        # code...
+    }
+
+    public function clear()
+    {
+        # code...
+    
+    }
+    public function forever()
+    {
+        # code...
+    }
+}
