f9a8fc4bdc10e0e5022a97b6f9a232ef0ba815fc
begimov <begimov@gmail.com>
2018-04-18 12:24:21 +0300

    Guest middleware created and applied to routes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,14 @@ $route->get('/', 'App\Controllers\HomeController::index')->setName('home');
 
 $route->group('', function ($route) {
     $route->get('/dashboard', 'App\Controllers\DashboardController::index')->setName('dashboard');
+
+    $route->post('/auth/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
 })->middleware($container->get(App\Middleware\Authenticated::class));
 
-$route->group('/auth', function ($route) {
-    $route->get('/login', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
-    $route->post('/login', 'App\Controllers\Auth\LoginController::login');
-    $route->post('/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
+$route->group('', function ($route) {
+    $route->get('/auth/login', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
+    $route->post('/auth/login', 'App\Controllers\Auth\LoginController::login');
 
-    $route->get('/register', 'App\Controllers\Auth\RegisterController::index')->setName('auth.register');
-    $route->post('/register', 'App\Controllers\Auth\RegisterController::register');
-});
+    $route->get('/auth/register', 'App\Controllers\Auth\RegisterController::index')->setName('auth.register');
+    $route->post('/auth/register', 'App\Controllers\Auth\RegisterController::register');
+})->middleware($container->get(App\Middleware\Guest::class));
