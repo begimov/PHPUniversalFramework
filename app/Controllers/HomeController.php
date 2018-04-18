2d263c6dd940ff62facdaf5a6c04ce9346795570
begimov <begimov@gmail.com>
2018-04-18 13:11:12 +0300

    CookieJar's set method implemented as well as needed prop added

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,18 +4,23 @@ namespace App\Controllers;
 
 use App\Views\View;
 use App\Auth\Auth;
+use App\Cookie\CookieJar;
 
 class HomeController
 {
     protected $view;
+    protected $cookie;
 
-    public function __construct(View $view)
+    public function __construct(View $view, CookieJar $cookie)
     {
         $this->view = $view;
+        $this->cookie = $cookie;
     }
 
     public function index($request, $response)
     {
+        $this->cookie->set('name', 'value');
+        
         return $this->view->render($response, 'home.twig');
     }
 }
--- app/Cookie/CookieJar.php
+++ app/Cookie/CookieJar.php
@@ -4,9 +4,22 @@ namespace App\Cookie;
 
 class CookieJar
 {
+    protected $path = '/';
+
+    protected $domain = null;
+
+    protected $secure = false;
+
+    protected $httpOnly = true;
+
     public function set($name, $value, $minutes = 60)
     {
-        //
+        $expiry = time() + ($minutes * 60);
+
+        setcookie(
+            $name, $value, $expiry, 
+            $this->path, $this->domain, $this->secure, $this->httpOnly
+        );
     }
 
     public function get()
