a45fcb42bb2b487c42002acfc90cda799775a517
begimov <begimov@gmail.com>
2018-04-18 11:20:08 +0300

    DashboardController created

---
+++ app/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Views\View;
+use App\Auth\Auth;
+
+class DashboardController
+{
+    protected $view;
+
+    public function __construct(View $view)
+    {
+        $this->view = $view;
+    }
+
+    public function index($request, $response)
+    {
+        return $this->view->render($response, 'dashboard.twig');
+    }
+}
